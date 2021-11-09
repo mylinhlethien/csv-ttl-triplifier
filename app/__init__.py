5a82cf8c9bdce29cde427c2363053d9acfabd6a5
My-Linh Le Thien <mylinh.lethien@gmail.com>
2021-11-09 10:51:39 +0100

    upload file form

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import sys
 
 
 def create_app():
@@ -6,6 +8,24 @@ def create_app():
 
     @app.route('/')
     def homepage():
-        return render_template('homepage.html')
+        return render_template("homepage.html")
+
+    # @app.route("/uploads/<filename>")
+    # def uploaded_file(filename):
+    #    filename_processed = 'processed' + '-' + filename
+    #    return send_from_directory("downloads", filename, as_attachment=True, attachment_filename=filename_processed)
+
+    @app.route('/convertFile', methods=['POST'])
+    def convertFile():
+        if request.method == 'POST':
+            uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(os.path.join("test", uploaded_file.filename))
+        # return redirect(url_for('uploaded_file', filename=filename))
+
+        result = request.form
+        print(result, file=sys.stderr)
+
+        return "<p>SUCCESS</p>"
 
     return app
